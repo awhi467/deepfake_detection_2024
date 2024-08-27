e69116c0d23da48666bf61ebbe08c69016cc3409
awhi467 <awhi467@aucklanduni.ac.nz>
2024-08-27 22:42:31 +1200

    1024 tests

--- MATLAB Experiments/Bispectral Analysis/generate_data.m
+++ MATLAB Experiments/Bispectral Analysis/generate_data.m
@@ -1,3 +1,7 @@
+% Script for generating input data for classifying between natural and
+% synthetic speech. Features are based on the axial and radial integrated
+% bicoherence/skewness function. 
+
 clear;
 addpath('functions');
 addpath('phase_unwrap')
@@ -12,70 +16,79 @@ addpath('..\Speech samples\LJ_synthetic')
 addpath('..\Speech samples\LJ_natural')
 
 % Specify directory of .wav files
-path_in = ['..\Speech samples\LJ_natural'];
-wav_files = dir(strcat(path_in,'\*.wav'));
+natural_path = '..\Speech samples\LJ_natural';
+natural_wavs = dir(strcat(natural_path,'\*.wav'));
+synthetic_path = '..\Speech samples\LJ_synthetic';
+synthetic_wavs = dir(strcat(synthetic_path,'\*.wav'));
 
 % Params 
 nlag = 25;
-nsamp = 1024;
+nsamp = 256;
 overlap = 50;
-nfft = 1024;
+nfft = 256;
 wind = hamming(nfft);
 fs= 22050;
 
 % Define pre-emphasis filter
 pe = tf([1,-0.95],1,1/fs,'Variable','z^-1');
 
+% Initialise data matrices
 f_bic_ax = [];
 f_sk_ax = [];
 f_bic_rad = [];
 f_sk_rad = [];
+%f_moments = [];
 
-% Loop through .wav files and compute features
-for i=1:size(wav_files,1)
-
-    % Print filename
-    disp(wav_files(i).name)
-
-    % Read file and pre-emphasise
-    [s,fs] = audioread(wav_files(i).name);
-    s = lsim(pe,s);
+for is_synthetic=0:1
     
-    % Bicoherence
-    [sk,bic,axis] = bicoherence_modified(s,nfft,wind,nsamp,overlap);
-
-    % Principal domain
-    bicp = bic(nfft/2+1:end, nfft/2+1:end).*triu(ones(nfft/2)).*fliplr(triu(ones(nfft/2)));
-    skp = sk(nfft/2+1:end, nfft/2+1:end).*triu(ones(nfft/2)).*fliplr(triu(ones(nfft/2)));
-
-    % Axial integral - limits right?
-    bic_ax = trapz(bicp,1);
-    sk_ax = trapz(skp,1);
-
-    % Radial integral (need to test)
-    bic_rad = compute_radial_integral(bicp,nfft,nfft/2);
-    sk_rad = compute_radial_integral(skp,nfft,nfft/2);
+    % First loop through natural wavs, then synthetic wavs
+    if is_synthetic
+        wav_files = synthetic_wavs;
+    else
+        wav_files = natural_wavs;
+    end
     
-    % Compute the first four statistical moments of mag/phase
-    %B_mag = abs(bic);     %or abs(B)
-    %B_ph = angle(bic);
-    %[B_mag_m1,B_mag_m2,B_mag_m3,B_mag_m4] = compute_moments(B_mag);
-    %[B_ph_m1,B_ph_m2,B_ph_m3,B_ph_m4] = compute_moments(B_ph);
-    
-    % Define feature vectors
-    %f_moments = [f_moments; B_mag_m1,B_mag_m2,B_mag_m3,B_mag_m4,B_ph_m1,B_ph_m2,B_ph_m3,B_ph_m4];
-    f_bic_ax = [f_bic_ax;abs(bic_ax)];
-    f_sk_ax = [f_sk_ax;abs(sk_ax)];
-    f_bic_rad = [f_bic_rad;abs(bic_rad)];
-    f_sk_rad = [f_sk_rad;abs(sk_rad)];
-    
-end
+    % Loop through .wav files and compute features
+    for i=1:size(wav_files,1)
 
-% Reduce the size to the first quadrant, normalise magnitudes
-function y = process(x,nfft)
-    y = x(nfft/2:end, nfft/2:end);
-    [min,max] = bounds(abs(y(:)));
-    y = (y-min)/(max-min);
+        % Print filename
+        disp(wav_files(i).name)
+
+        % Read file
+        [s,fs] = audioread(wav_files(i).name);
+        
+        % Pre-emphasise
+        s = lsim(pe,s);
+
+        % Bicoherence and skewness
+        [sk,bic,axis] = bicoherence_modified(s,nfft,wind,nsamp,overlap);
+
+        % Principal domain
+        bicp = bic(nfft/2+1:end, nfft/2+1:end).*triu(ones(nfft/2)).*fliplr(triu(ones(nfft/2)));
+        skp = sk(nfft/2+1:end, nfft/2+1:end).*triu(ones(nfft/2)).*fliplr(triu(ones(nfft/2)));
+
+        % Axial integral 
+        bic_ax = trapz(bicp,1);
+        sk_ax = trapz(skp,1);
+
+        % Radial integral
+        bic_rad = compute_radial_integral(bicp,nfft,nfft/2);
+        sk_rad = compute_radial_integral(skp,nfft,nfft/2);
+
+        % Compute the first four statistical moments of mag/phase
+        %B_mag = abs(bic);     %or abs(B)
+        %B_ph = angle(bic);
+        %[B_mag_m1,B_mag_m2,B_mag_m3,B_mag_m4] = compute_moments(B_mag);
+        %[B_ph_m1,B_ph_m2,B_ph_m3,B_ph_m4] = compute_moments(B_ph);
+
+        % Add feature vectors to data matrices
+        %f_moments = [f_moments; B_mag_m1,B_mag_m2,B_mag_m3,B_mag_m4,B_ph_m1,B_ph_m2,B_ph_m3,B_ph_m4];
+        f_bic_ax = [f_bic_ax;is_synthetic,abs(bic_ax)];
+        f_sk_ax = [f_sk_ax;is_synthetic,abs(sk_ax)];
+        f_bic_rad = [f_bic_rad;is_synthetic,abs(bic_rad)];
+        f_sk_rad = [f_sk_rad;is_synthetic,abs(sk_rad)];
+
+    end
 end
 
 % Compute the first four statistical moments
@@ -98,8 +111,47 @@ function rad = compute_radial_integral(B,nfft,rad_points)
     end
 end
 
+% Interpolation for the radial integral
+% B(k,ak) = pB(k,ceil(ak))+(1-p)B(k,floor(ak))
+function Brad = interpolate(B,alpha)
+    [K1,K2] = size(B);
+    k1 = 1:K1;
+    k2 = 1:K2;
+    p = alpha*k1-floor(alpha*k1);
+    index1 = ceil(alpha*k1);
+    index2 = floor(alpha*k1);
+    index1(index1==0)=1;
+    index2(index2==0)=1;
+    Brad = p*B(k1,index1)+(1-p)*B(k1,index2);
+end
 
-% Radial and axial integrals, computed over the principal domain
+% Bisepctrum energy normalisation - not needed if using bicoherence/skewness
+function Bn = energy_normalise(B)
+    absB = abs(B);
+    vol = sum(absB(:));
+    Bn = B/vol;
+end
+
+    % Complex bicepstrum
+    %Blog = log(abs(sk)) + 1j*phase_unwrap(angle(B));
+    %b = ifft2(Blog);
+    %b(1,1)=b(1,1)/3;
+
+    % Compute linear and nonlinear complex bicepstrum
+    %bL = zeros(nfft);
+    %bL(1,:) = b(1,:);
+    %bL(:,1) = b(:,1);
+    %bL(1:nfft+1:end) = diag(b);
+    %bN = b - bL;
+    %bH = b(1,:);
+
+    % Compute linear and nonlinear bispectrum
+    %BL = exp(fft2(bL));
+    %BN = exp(fft2(bN));
+    %BL = process(BL,nfft);
+    %BN = process(BN,nfft);
+    
+    % Radial and axial integrals, computed over the principal domain
 %function [rad, ax] = integration_measures(B,nfft,rad_points)
     
 
@@ -123,42 +175,3 @@ end
     %rad = Ir;
     %ax = Ia;
 %end
-
-% Interpolation for the radial integral
-% B(k,ak) = pB(k,ceil(ak))+(1-p)B(k,floor(ak))
-function Brad = interpolate(B,alpha)
-    [K1,K2] = size(B);
-    k1 = 1:K1;
-    k2 = 1:K2;
-    p = alpha*k1-floor(alpha*k1);
-    index1 = ceil(alpha*k1);
-    index2 = floor(alpha*k1);
-    index1(index1==0)=1;
-    index2(index2==0)=1;
-    Brad = p*B(k1,index1)+(1-p)*B(k1,index2);
-end
-
-    % Complex bicepstrum
-    %Blog = log(abs(sk)) + 1j*phase_unwrap(angle(B));
-    %b = ifft2(Blog);
-    %b(1,1)=b(1,1)/3;
-
-    % Compute linear and nonlinear complex bicepstrum
-    %bL = zeros(nfft);
-    %bL(1,:) = b(1,:);
-    %bL(:,1) = b(:,1);
-    %bL(1:nfft+1:end) = diag(b);
-    %bN = b - bL;
-    %bH = b(1,:);
-
-    % Compute linear and nonlinear bispectrum
-    %BL = exp(fft2(bL));
-    %BN = exp(fft2(bN));
-    %BL = process(BL,nfft);
-    %BN = process(BN,nfft);
-
-function Bn = energy_normalise(B)
-    absB = abs(B);
-    vol = sum(absB(:));
-    Bn = B/vol;
-end
